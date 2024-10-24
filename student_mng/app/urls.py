1669aa9b17361cdebaf97b0aeaf450470e4e9423
agnaey <agnaeyviswanath1234@gmail.com>
2024-10-24 13:36:56 +0530

    edit added

--- student_mng/app/urls.py
+++ student_mng/app/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns=[
     path('index',views.index_page),
     path('add',views.add_std),
+    path('disp',views.disp_std),
+    path('edit/<id>',views.edit_std)
+
 
 ]
--- student_mng/app/views.py
+++ student_mng/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from . models import Students
 
@@ -9,6 +9,33 @@ def index_page(request):
     return render(request, 'index.html',{'data':data})
 
 def add_std(req):
-    return render(req,'add.html')
+    if req.method=='POST':
+        roll=req.POST['roll']
+        std_name=req.POST['name']
+        std_age=req.POST['age']
+        std_email=req.POST['email']
+        std_phone=req.POST['phone']
+        data=Students.objects.create(roll_no=roll,name=std_name,age=std_age,email=std_email,phone=std_phone)
+        data.save()
+        return redirect(disp_std)
+    else:
+        return render(req,'add.html')
+
+def disp_std(req):
+    data=Students.objects.all()
+    return render(req,'display_std.html',{'data':data})
+
+def edit_std(req,id):
+    data=Students.objects.get(pk=id)
+    if req.method=='POST':
+        roll=req.POST['roll']
+        std_name=req.POST['name']
+        std_age=req.POST['age']
+        std_email=req.POST['email']
+        std_phone=req.POST['phone']
+        Students.objects.filter(pk=id).update(roll_no=roll,name=std_name,age=std_age,email=std_email,phone=std_phone)
+        return redirect(disp_std)
+    else:
+        return render(req,'edit_std.html',{'data':data})
 
 
